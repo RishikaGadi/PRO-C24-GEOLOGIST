dd78d00a00e6e1d06c66443906488be196b508f2
RishikaGadi <79797889+RishikaGadi@users.noreply.github.com>
2021-10-23 11:21:09 -0400

    Add files via upload

--- Rubber.js
+++ Rubber.js
@@ -2,9 +2,14 @@ class Rubber{
 	constructor(x,y,r)
 	{
 	// assign options to the rubber ball
+	var options = {
+		'density':1,
+        'friction': 5,
+        'restitution':0.3
+	}
 		this.x=x;
 		this.y=y;
-		this.r=r
+		this.r=r;
 		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
 		World.add(world, this.body);
 
@@ -19,6 +24,7 @@ class Rubber{
 			stroke("black");
 			fill("darkblue");
 			//draw the ellipse here to display the rubber ball
+			ellipse(0,0, this.r, this.r);
 
 			pop()
 	}
